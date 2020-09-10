c53653a488eb461040d7219588490f5381cdb7b3
Andy Tran <foolmonkey99@gmail.com>
2020-09-09 20:33:54 -0600

    Add project and player sprite with script

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    float moveSpeed = 300.0f;
+    float movement = 0.0f;
+
+    // Start is called before the first frame update
+    void Start () {
+    }
+
+    private void FixedUpdate () {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        movement = Input.GetAxisRaw ("Horizontal");
+    }
+}

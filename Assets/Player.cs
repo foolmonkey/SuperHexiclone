94127a2978057c34061b7e80b4bdee8dfd8ea384
Andy Tran <foolmonkey99@gmail.com>
2020-09-09 21:27:00 -0600

    Add Rotator class with player collision event

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     float moveSpeed = 300.0f;
     float movement = 0.0f;
 
     // Start is called before the first frame update
-    void Start () {
+    void Start()
+    {
     }
 
-    private void FixedUpdate () {
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void FixedUpdate()
+    {
         transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
     }
 
     // Update is called once per frame
-    void Update () {
-        movement = Input.GetAxisRaw ("Horizontal");
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
     }
 }
